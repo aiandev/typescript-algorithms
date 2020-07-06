0bf647d181e3e3e1d00d2f279c3f176d76ac1868
Aian <aian.abdulaziz@feel-it-services.com>
2020-07-06 22:19:05 +0300

    Feature : add Comparator class in tools

---
+++ src/tools/__test__/Comparator.test.ts
@@ -0,0 +1,22 @@
+import Comparator from '../Comparator';
+
+describe('Comparator', () => {
+  it('should compare with default comparator function', () => {
+    const comparator = new Comparator();
+
+    expect(comparator.equal(0, 0)).toBeTruthy();
+    expect(comparator.equal(0, 1)).toBeFalsy();
+    expect(comparator.lessThan(1, 2)).toBeTruthy();
+    expect(comparator.lessThan(-1, 2)).toBeTruthy();
+    expect(comparator.lessThan(10, 2)).toBeFalsy();
+    expect(comparator.lessThanOrEqual(10, 2)).toBeFalsy();
+    expect(comparator.lessThanOrEqual(1, 1)).toBeTruthy();
+    expect(comparator.lessThanOrEqual(0, 0)).toBeTruthy();
+    expect(comparator.greaterThan(0, 0)).toBeFalsy();
+    expect(comparator.greaterThan(5, 0)).toBeTruthy();
+    expect(comparator.greaterThanOrEqual(5, 0)).toBeTruthy()
+    expect(comparator.greaterThanOrEqual(2, 2)).toBeTruthy()
+    expect(comparator.greaterThanOrEqual(2, 10)).toBeFalsy();
+  });
+
+});
